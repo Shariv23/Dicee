3157746f9553cf8f8b43cfc44a300c734996e838
Shariv23 <128408255+Shariv23@users.noreply.github.com>
2024-02-23 18:38:52 +0200

    upload

--- dicee.js
+++ dicee.js
@@ -15,11 +15,11 @@ document.querySelector(".img2").setAttribute("src",res2);
 console.log(res2);
 
 if (randomNum>randomNum2){
-    document.querySelector("h1").textContent = "Player 1 Wins!";
+    document.querySelector("h1").textContent = "🚩Player 1 Wins!";
 }
 else if (randomNum<randomNum2){
-    document.querySelector("h1").textContent = "Player 2 Wins!";
+    document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
 }
 else{
-    document.querySelector("h1").textContent = "Draw!";
+    document.querySelector("h1").textContent = "🔃 Draw!";
 }
